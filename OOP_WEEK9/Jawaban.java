bf0e0b1e398729a2319032e478354d986ef71efd
Andre William <andrecool.cool@gmail.com>
2021-06-05 14:18:11 +0700

    add w9

---
+++ OOP_WEEK9/Jawaban.java
@@ -0,0 +1,15 @@
+
+//jawaban lab oop java week 9
+
+//1.
+//        i from A is 40
+//        i from A is 60
+//        i from B is 60
+
+//2.
+//        Person
+//        Student
+
+//3.
+//        Person
+//        Person
